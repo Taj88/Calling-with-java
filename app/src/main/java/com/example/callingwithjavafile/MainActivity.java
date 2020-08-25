59969ec0cd569907087499e0a6c38b21e3b1d5d7
Taj88 <taj16@cse.pstu.ac.bd>
2020-08-25 20:26:43 +0600

    First try

---
+++ app/src/main/java/com/example/callingwithjavafile/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.callingwithjavafile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.jar.Attributes;
+
+public class MainActivity extends AppCompatActivity {
+private TextView variable;
+private TextView varage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        variable= (TextView)findViewById(R.id.Name);
+        variable.setText("Taj");
+
+      varage=(TextView) findViewById(R.id.Name2nd);
+      varage.setText("27");
+
+
+
+
+    }
+}
